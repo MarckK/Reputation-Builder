9dc2645945fcc2f1d9c30bf6a1a451affb1b6136
Kara de la Marck <karadelamarck@gmail.com>
2016-06-22 06:42:49 +0100

    Feat: functionality for Get Unanswered Questions and View Top Answerers for a Question

--- js/app.js
+++ js/app.js
@@ -40,6 +40,14 @@
     * @param {Object} question: Question object from SO API response
     * @returns {String} HTML markup to be added to the page for given question
     */
+    // title, link, username, reputation
+    return (
+      '<div>Question Title: ' + question.title + '</div>' +
+      '<div>Link: <a href="' + question.link + '">Click here</a></div>' +
+      '<div>Owner: ' + question.owner.display_name + '</div>' +
+      '<div>Repution: ' + question.owner.reputation + '</div>' +
+      '</br>'
+    )
   }
 
   function getAnswererMarkup (answerer) {
@@ -47,6 +55,13 @@
     * @param {Object} answerer: User object from SO API response
     * @returns {String} HTML markup to be added to the page for given user
     */
+    return (
+      '<div>User: ' + answerer.user.display_name + '</div>' +
+      '<div>Profile Link: <a href="' + answerer.user.link + '">Click here</a></div>' +
+      '<div>Avatar: <img src="' + answerer.user.profile_image + '" /></div>' +
+      '<div>Repution: ' + answerer.user.reputation + '</div>' +
+      '</br>'
+    )
   }
 
   /* ------------------------------------------------------------
@@ -59,14 +74,85 @@
     * @param {String} tags: Comma separated string of SO tags for search
     * @returns {void}
     */
-  }
 
+    var query = {
+      order: 'desc',
+      sort: 'activity',
+      tagged: tags,
+      site: 'stackoverflow'
+    }
+
+    var baseUrl = 'https://api.stackexchange.com'
+    var path = '/2.2/questions/unanswered'
+    var queryString = Object.keys(query).map(function (key) {
+      return key + '=' + query[key]
+    }).join('&')
+
+    console.log(queryString);
+    console.log(baseUrl + path + '?' + queryString);
+    console.log('Me get Unanswered: https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&tagged=' + tags + '&site=stackoverflow')
+
+    var xhr = new XMLHttpRequest()
+
+    // xhr.onreadystatechange = function () {
+    //   if (xhr.readyState === 4 && xhr.status === 200)
+    //   console.log(xhr.response)
+    // }
+
+    xhr.addEventListener('load', function() {
+      if(xhr.status === 200) {
+        var response = JSON.parse(xhr.response)
+        var summary = getResultsSummaryMarkup(tags, response.items.length)
+        var questions = response.items.map(getQuestionMarkup)
+
+        updateResultsSummary(summary)
+        updateResultsBody(questions)
+      }
+    })
+    xhr.open('GET', baseUrl + path + '?' + queryString)
+    xhr.send()
+  }
   function getInspiration (tag) {
     /**
     * Makes AJAX request to get top answerers for tag and updates results
     * @param {String} tag: SO tag for which to get top answerers
     * @returns {void}
     */
+
+    var query = {
+      site: 'stackoverflow'
+    }
+
+    var baseUrl = 'https://api.stackexchange.com'
+    var path = '/2.2/tags/' + tag + '/top-answerers/all_time'
+
+    var queryString = Object.keys(query).map(function (key) {
+      return key + '=' + query[key]
+    })
+
+    console.log(queryString);
+    console.log(baseUrl + path + '?' + queryString);
+    console.log('Me topanswerers: https://api.stackexchange.com/2.2/tags/' + tag + '/top-answerers/all_time?site=stackoverflow')
+
+    var xhr = new XMLHttpRequest()
+
+    // xhr.onreadystatechange = function () {
+    //   if (xhr.readyState === 4 && xhr.status === 200)
+    //   console.log(xhr.response)
+    // }
+
+    xhr.addEventListener('load', function() {
+      if(xhr.status === 200) {
+        var response = JSON.parse(xhr.response)
+        var summary = getResultsSummaryMarkup(tag, response.items.length)
+        var answerers = response.items.map(getAnswererMarkup)
+
+        updateResultsSummary(summary)
+        updateResultsBody(answerers)
+      }
+    })
+    xhr.open('GET', baseUrl + path + '?' + queryString)
+    xhr.send()
   }
 
   /* ------------------------------------------------------------
